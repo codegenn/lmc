012658c167c93bd2c43e9d7cff15f5268d42493d
Tracy Leung <tracy.leung.1991@gmail.com>
2020-05-25 16:56:18 +0700

    + edit email from + UI change

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,12 +2,12 @@ class UserMailer < ApplicationMailer
   def order_for_user(order)
     @order = order
     subj = 'Thanks for Purchase'
-    mail(to: @order.email, bcc: 'info@lmcation.com', subject: subj, from: "info@lmcation.com")
+    mail(to: @order.email, bcc: 'info@lmcation.com', subject: subj, from: Rails.application.secrets.gmail_email)
   end
 
   def message_for_admin(message)
     @message = message
     subj = 'You have a new message'
-    mail(to: 'info@lmcation.com', subject: subj, from: "info@lmcation.com")
+    mail(to: 'info@lmcation.com', subject: subj, from: Rails.application.secrets.gmail_email)
   end
 end
