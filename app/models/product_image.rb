30b6e2a9882dc3fbbe5bac4b2b68fcd35ef75b1d
Tracy Leung <tracy.leung.1991@gmail.com>
2020-09-07 16:17:22 +0700

    Fix email

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -5,7 +5,7 @@ class ProductImage < ActiveRecord::Base
     message: 'must be a URL for GIF, JPG, JPEG or PNG image'
   }
 
-  has_attached_file :pimage
+  has_attached_file :pimage, :s3_protocol => :https
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :pimage, :content_type => /image/
 
