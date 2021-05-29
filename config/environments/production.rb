971e0c245c9ad8c194ee07f44f0d6cf7320f8668
nghichcode <nghichcode@gmail.com>
2021-05-30 01:00:35 +0700

    Improve perf products page S1.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,8 @@ Rails.application.configure do
   }
   config.paperclip_defaults = {
     storage: :s3,
+    url: ':s3_domain_url',
+    path: '/:class/:attachment/:id_partition/:style/:filename',
     s3_credentials: {
       bucket: Rails.application.secrets.bucket,
       access_key_id: Rails.application.secrets.access_key_id,
