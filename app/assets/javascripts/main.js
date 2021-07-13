539c07f097e3f12196e873bcc37bd29218d0fed1
Cam.nguyenhong <hongcam941997@gmail.com>
2021-07-13 16:51:16 +0700

    fix text

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -41,10 +41,16 @@ $(function () {
     });
 
     $(".text-split").each(function() {
+        var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
         var data = $(this).text();
-        console.log(data);
-        if (data.length > 90) {
-            $(this).text(data.slice(0, 90) + ' ...');
+        if(width < 1600 && width > 1300 ) {
+            $(this).text(data.slice(0, 80) + '...');
+        } else if(width == 1200) {
+            $(this).text(data.slice(0, 69) + '...');
+        } else {
+            if (data.length > 90) {
+                $(this).text(data.slice(0, 90) + '...');
+            }
         }
     });
 
