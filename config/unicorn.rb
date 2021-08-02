da6683dd43557870488486367ef2002551a21fad
Cam.nguyenhong <hongcam941997@gmail.com>
2021-08-02 21:46:32 +0700

    Fix revert

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,13 +1,20 @@
-#đây là thư mực app của bạn
-root = "/var/www/lmc/current"
-working_directory root
-#pid của unicorn khi chạy
-pid "#{root}/tmp/pids/unicorn.pid"
-#log
-stderr_path "#{root}/log/unicorn.error.log"
-stdout_path "#{root}/log/unicorn.access.log"
+# set path to application
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
+working_directory app_dir
 
-#chạy với sock
-listen "#{root}/shared/sockets/unicorn.sock"
+
+# Set unicorn options
 worker_processes 2
+preload_app true
 timeout 30
+
+# Set up socket location
+listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+
+# Logging
+stderr_path "#{shared_dir}/log/unicorn.stderr.log"
+stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+
+# Set master PID location
+pid "#{shared_dir}/pids/unicorn.pid"
