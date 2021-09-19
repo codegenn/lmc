0463dc924ec29a421b7f139c29030ade0fb90ce3
Cam.nguyenhong <hongcam941997@gmail.com>
2021-09-19 22:32:34 +0700

    test....

--- app/controllers/api/v1/spp_controller.rb
+++ app/controllers/api/v1/spp_controller.rb
@@ -10,8 +10,8 @@ module Api
         order = Order.find_by_id(@order_id)
         Rails.logger.info("order: #{order.grand_total}")
         return render json: template_json(0, "Không Thành Công") if order.nil?
-        order.payment_status = @data_respon_spp["payment_status"]
-        update_status(order, @data_respon_spp["payment_status"])
+        order.payment_status = "#{@data_respon_spp["transaction_status"]}_#{@data_respon_spp["transaction_type"]}_#{@data_respon_spp["payment_method"]}"
+        update_status(order, @data_respon_spp["transaction_status"])
         if @data_respon_spp["amount"].to_i == order.grand_total.to_i*100 && order.save
           Rails.logger.info("amount: #{@data_respon_spp["amount"]}")
           Rails.logger.info("=====save data=====")
