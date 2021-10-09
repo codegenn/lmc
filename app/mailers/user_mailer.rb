b249de39e938dbafaa13834597fe4348a4b8cf85
Cam.nguyenhong <hongcam941997@gmail.com>
2021-10-09 23:19:18 +0700

    fix cart

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
   def order_for_user(order)
     @order = order
+    @payment_method = payment_method(@order)
     subj = 'Thanks for Purchase'
     mail(to: @order.email, bcc: 'info@lmcation.com', subject: subj, from: Rails.application.secrets.gmail_email)
   end
@@ -9,5 +10,15 @@ class UserMailer < ApplicationMailer
     @message = message
     subj = 'You have a new message'
     mail(to: 'info@lmcation.com', subject: subj, from: Rails.application.secrets.gmail_email)
+  end
+
+  def payment_method(order)
+    if order.payment_method == 'COD'
+      return 'Thanh toán khi giao hàng (COD)'
+    elsif order.payment_method.include?('FUNDIIN')
+      return 'Chuyển Khoản FUNDIIN'
+    elsif order.payment_method.include?("Shopee pay")
+      return 'Chuyển Khoản Ví ShopeePay'
+    end
   end
 end
