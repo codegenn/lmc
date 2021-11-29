8818ffd7b4669ddc1cd565f97b8694eae3ddbe6a
hvu0163 <53392914+hvu0163@users.noreply.github.com>
2021-11-29 21:04:23 +0700

    update activeadmin

--- app/admin/banner.rb
+++ app/admin/banner.rb
@@ -1,12 +1,12 @@
 ActiveAdmin.register Banner do
-    form action: '/upload-banner', method: 'post', enctype: 'multipart/form-data' do
-        input :submit, type: :submit, value: 'Upload'
-        label 'Thay đường link'
-        input type: 'text', name: 'link'
-        label 'Chọn ảnh cho banner desktop'
-        input type: 'file', name: 'filedesktop', accept: 'image/jpg, image/jpeg, image/png'
-        label 'Chọn ảnh cho banner mobile'
-        input type: 'file', name: 'filemobile', accept: 'image/jpg, image/jpeg, image/png'
-    end
+    # form action: '/upload-banner', method: 'post', enctype: 'multipart/form-data' do
+    #     input :submit, type: :submit, value: 'Upload'
+    #     label 'Thay đường link'
+    #     input type: 'text', name: 'link'
+    #     label 'Chọn ảnh cho banner desktop'
+    #     input type: 'file', name: 'filedesktop', accept: 'image/jpg, image/jpeg, image/png'
+    #     label 'Chọn ảnh cho banner mobile'
+    #     input type: 'file', name: 'filemobile', accept: 'image/jpg, image/jpeg, image/png'
+    # end
 end
   
