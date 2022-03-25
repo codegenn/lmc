ebc2a7c1807fc1442fdc5269a7d554e5628cc9f9
harold055 <zohaib055@gmail.com>
2022-03-25 10:46:25 +0700

    Fix title

--- app/controllers/foundations_controller.rb
+++ app/controllers/foundations_controller.rb
@@ -17,7 +17,7 @@ class FoundationsController < ApplicationController
   def show
     @blog = Foundation.friendly.find(params[:id])
     meta_data(
-        "lmcation.com, lmcation",
+        @blog.title,
         @blog.title,
         Cloudinary::Utils.cloudinary_url(@blog.f_image_url),
         "https://www.lmcation.com/#{I18n.locale.to_s}/foundations/#{@blog.slug}"
