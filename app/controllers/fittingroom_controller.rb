8d033968c5acd57554c86870269b8513861aee04
Cam nguyen <hongcam941997@gmail.com>
2022-10-07 16:39:24 +0700

    add pass

--- app/controllers/fittingroom_controller.rb
+++ app/controllers/fittingroom_controller.rb
@@ -1,4 +1,11 @@
 class FittingroomController < ApplicationController
-    def index; end
+  before_action :authenticate_user!
+  def index
+    if current_user.email == "lmcation-demo@gmail.com" && current_user.username == "LMCATION"
+      return
+    else
+      redirect_to products_path
+    end
   end
+end
   
