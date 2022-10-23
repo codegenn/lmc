7b5eb30660b346dd79e7bcfef0004d9fc046c685
Cam nguyen <hongcam941997@gmail.com>
2022-10-23 20:28:11 +0700

    update kiot

--- app/controllers/users/social_signup_controller.rb
+++ app/controllers/users/social_signup_controller.rb
@@ -1,13 +1,12 @@
-class Users::SocialSignupController < UsersController
+# class Users::SocialSignupController < UsersController
  
-  def create
-    super do |resource|
-      binding.pry
-      # If SNS account doesn't have associated email, user need to verify email.
-      # Encrypted password's presence is required to authenticate user by other micro-services.
-      # resource.password = Devise.friendly_token[8, 20]
-      # resource.skip_confirmation! if @sns_email
-      # resource.social_credentials.build(provider: sns_auth["provider"], uid: sns_auth["uid"])
-    end
-  end
-end
+#   def create
+#     super do |resource|
+#       # If SNS account doesn't have associated email, user need to verify email.
+#       # Encrypted password's presence is required to authenticate user by other micro-services.
+#       # resource.password = Devise.friendly_token[8, 20]
+#       # resource.skip_confirmation! if @sns_email
+#       # resource.social_credentials.build(provider: sns_auth["provider"], uid: sns_auth["uid"])
+#     end
+#   end
+# end
