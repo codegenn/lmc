fddbc85384df6d0d365c42d8ac95e1b4868f6cdb
camnguyen <hongcam941997@gmail.com>
2023-04-25 22:51:21 +0700

    Fix line and fix login partner (#97)

--- app/controllers/partner_users/sessions_controller.rb
+++ app/controllers/partner_users/sessions_controller.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
 class PartnerUsers::SessionsController < Devise::SessionsController
-  before_action :configure_sign_in_params, only: [:create]
+  # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  def new
-    super
-    flash.now[:error] = I18n.t("partner.signin_error")
-  end
+  # def new
+  #   super
+  #   flash.now[:error] = I18n.t("partner.signin_error")
+  # end
 
   # POST /resource/sign_in
-  def create
-    super
-  end
+  # def create
+  #   super
+  # end
 
   # DELETE /resource/sign_out
   # def destroy
@@ -22,8 +22,8 @@ class PartnerUsers::SessionsController < Devise::SessionsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :address, :info])
+  def sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   end
 
   def after_sign_in_path_for(resource)
