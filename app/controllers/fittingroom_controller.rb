c9253b49212a1badf727fcd3f7cac88cddb165f3
Nguyễn Cẩm <hongcam941997@gmail.com>
2023-11-19 17:40:53 +0700

    add title and GA

--- app/controllers/fittingroom_controller.rb
+++ app/controllers/fittingroom_controller.rb
@@ -1,5 +1,11 @@
 class FittingroomController < ApplicationController
   def index
+    meta_data(
+      "LMCATION - fitting-room",
+      "Đồ Mặc Nhà - Đồ Ngủ, Gym-to-Swim, Đồ Bơi, Đồ Thể Thao",
+      "https://res.cloudinary.com/dbysq36qu/image/upload/v1622280133/main-logo-sm.png",
+      "https://www.lmcation.com/#{I18n.locale.to_s}",
+      "fitting-room"
+    )
   end
 end
-  
